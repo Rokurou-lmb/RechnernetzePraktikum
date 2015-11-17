a67a2469347e2dcdef2e084c80af5941ec3c46b9
abl459 <abl459@141.22.40.106>
2015-11-17 17:02:13 +0100

    ChatServer Loop angelegt ProtokollDefinitionen geschrieben

--- RechnernetzePraktikum/src/praktikum2/chat_server/ChatServer.java
+++ RechnernetzePraktikum/src/praktikum2/chat_server/ChatServer.java
@@ -1,5 +1,23 @@
 package praktikum2.chat_server;
 
-public class ChatServer {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+public class ChatServer implements Runnable{
+	Map<Object, String> _nicknameMapping;
+	Set<Object> _verteiler;
+	
+	public ChatServer() {
+		_nicknameMapping = new HashMap<>();
+		_verteiler = new HashSet<>();
+	}
+
+	@Override
+	public void run() {
+		while(true) {
+			
+		}
+	}
 }
